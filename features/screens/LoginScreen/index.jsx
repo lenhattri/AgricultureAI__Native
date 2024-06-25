15d6d28e714b3264f488140eaeb6959d2dce80f0
lenhattri <lenhattri7@gmail.com>
2024-06-25 18:21:52 +0700

    add jwt authentication

--- features/screens/LoginScreen/index.jsx
+++ features/screens/LoginScreen/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import { Div, Button, Text } from 'react-native-magnus';
-import { useDispatch, useSelector } from 'react-redux';
+import { Div, Button } from 'react-native-magnus';
+import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { userLoggedIn } from '../../../redux/currentUser/actions';
 import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser'
+import * as WebBrowser from 'expo-web-browser';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 WebBrowser.maybeCompleteAuthSession();
@@ -18,53 +18,70 @@ export default function LoginScreen() {
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
     scopes: ['profile', 'email'],
   });
+
   const handleSigninWithGoogle = async () => {
     console.log('handle sign in with google');
-    user = await AsyncStorage.getItem('@user')
-    console.log(user)
-    if (!user) {
-      if (response?.type === 'success') {
-        await getUserInfo(response.authentication.accessToken)
-      }
-    } else {
-      dispatch(userLoggedIn(user))
-      navigation.navigate('Home')
+    user = await AsyncStorage.getItem('@user');
+    console.log(user);
+    if (!user && response?.type === 'success') {
+      await getUserInfo(response.authentication.accessToken);
+    } else if (user) {
+      dispatch(userLoggedIn(JSON.parse(user)));
+      navigation.navigate('Home');
     }
-  }
+  };
+
   const getUserInfo = async (token) => {
     if (!token) {
       return;
     }
     try {
-      const response = await fetch(
+      const googleResponse = await fetch(
         'https://www.googleapis.com/userinfo/v2/me', {
         headers: {
           Authorization: `Bearer ${token}`,
         }
-      }
-      )
-      const user = await response.json()
-      console.log(user)
-      await AsyncStorage.setItem('@user', JSON.stringify(user))
-      await dispatch(userLoggedIn(user))
-    } catch (e) {
-      console.log(e)
-    }
-  }
-  const signInWithGoogle = async () => {
-    console.log("Hi")
-    try {
-      console.log("Start")
-      await promptAsync()
-      console.log(end)
-    } catch (e) {
-      console.log(e)
-    }
+      });
+      const googleUser = await googleResponse.json();
 
-  }
+      // Send Google user info to backend
+      const backendResponse = await fetch('http://localhost:3000/api/v1/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: googleUser }),
+      });
+      const { token: jwtToken, newUser } = await backendResponse.json();
+
+      // If the user is new, save the new user data
+      if (newUser) {
+        await AsyncStorage.setItem('@user', JSON.stringify(googleUser));
+      }
+
+      // Store JWT token and user info
+      await AsyncStorage.setItem('@jwtToken', jwtToken);
+      await AsyncStorage.setItem('@user', JSON.stringify(googleUser));
+
+      dispatch(userLoggedIn(googleUser));
+      navigation.navigate('Home');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await promptAsync();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
-    handleSigninWithGoogle()
-  }, [response])
+    handleSigninWithGoogle();
+  }, [response]);
+
   return (
     <Div flex={1} justifyContent='center' alignItems='center' bg="gray900">
       <Button alignSelf='center' onPress={signInWithGoogle}>Login with Google</Button>
